5fb31a9127cbb87c7d66dda1e1274601c0d70fe0
Thomas Piellard <thomas.piellard@consensys.net>
2023-05-05 15:46:12 +0200

    feat: apache-2.0 license + copyright

--- tmpl/template_verifier.go
+++ tmpl/template_verifier.go
@@ -1,6 +1,22 @@
 package tmpl
 
-const solidityVerifier = `pragma solidity ^0.8.0;
+const solidityVerifier = `
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2023 ConsenSys Software Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+pragma solidity ^0.8.0;
 
 pragma experimental ABIEncoderV2;
 
@@ -442,7 +458,7 @@ library PlonkVerifier {
     }
 
   function Verify(bytes memory proof, uint256[] memory public_inputs) 
-  internal returns(bool) {
+  internal view returns(bool) {
 
     uint256 gamma;
     uint256 beta;
